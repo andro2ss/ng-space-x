1f67fa56a8baf68ad21170471a86633f2d4346fe
andro2ss <a.brzeski@hotmail.com>
2022-05-07 19:22:15 +0200

    added eslint and prettier, prepared routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailsComponent } from './details/details.component';
+import { ListComponent } from './list/list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {
+    path: 'home/details',
+    component: DetailsComponent,
+  },
+  { path: 'home', component: ListComponent },
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+
+export const routingComponents = [
+  DetailsComponent,
+  ListComponent,
+  PageNotFoundComponent,
+];
