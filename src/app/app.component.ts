5b05d2ab9191179e4f4ff0ae68fe345c5b1c0758
andro2ss <a.brzeski@hotmail.com>
2022-05-10 15:45:23 +0200

    fiter and pagination logic completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { SpacexListService } from './services/spacex-list.service';
-import { SpaceX } from './models/spaceXList.model';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-  constructor(private spaceXListService: SpacexListService) {}
-  spaceXList?: SpaceX[];
-
-  ngOnInit(): void {
-    this.getSpaceXList();
-  }
-
-  private getSpaceXList() {
-    this.spaceXListService.getSpaceXList().subscribe({
-      next: response => {
-        this.spaceXList = response;
-        console.log(response);
-      },
-    });
-  }
+export class AppComponent {
+  constructor() {}
 }
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { BsLocaleService } from 'ngx-bootstrap/datepicker';
 import { PrimeNGConfig } from 'primeng/api';
+import { SpacexListService } from '../services/spacex-list.service';
+import { SpaceX } from '../models/spaceXList.model';
 
 @Component({
   selector: 'app-list',
@@ -8,16 +10,20 @@ import { PrimeNGConfig } from 'primeng/api';
   styleUrls: ['./list.component.scss'],
 })
 export class ListComponent implements OnInit {
+  spaceXList?: SpaceX[];
+  flights?: SpaceX[];
   successFly: boolean = false;
   nameFly: string = '';
   dateFly?: Date[];
   minDate: Date;
   maxDate: Date;
   locale = 'pl';
+  pagination: number = 1;
 
   constructor(
     private localeService: BsLocaleService,
-    private primengConfig: PrimeNGConfig
+    private primengConfig: PrimeNGConfig,
+    private spaceXListService: SpacexListService
   ) {
     this.minDate = new Date('2006-03-20T10:30:00+12:00');
     this.maxDate = new Date();
@@ -25,7 +31,86 @@ export class ListComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.getSpaceXList();
     this.localeService.use(this.locale);
     this.primengConfig.ripple = true;
+  }
+
+  showFlights() {
+    const tempDate = new Date();
+    this.flights = this.spaceXList
+      ?.sort((a, b) => {
+        return +new Date(b.date_utc) - +new Date(a.date_utc);
+      })
+      .filter(flight => {
+        const compareDate = new Date(flight.date_utc);
+        return compareDate <= tempDate;
+      })
+      .filter(flight => {
+        if (this.nameFly.length > 1) {
+          return flight.name === this.nameFly;
+        } else {
+          return flight;
+        }
+      })
+      .filter(flight => {
+        if (this.successFly) {
+          return flight.success;
+        } else {
+          return flight;
+        }
+      })
+      .filter(flight => {
+        if (this.dateFly) {
+          const dateFilterMin = new Date(this.dateFly[0]);
+          const dateFilterMax = new Date(this.dateFly[1]);
+          const tempDate = new Date(flight.date_utc);
+          return tempDate >= dateFilterMin && tempDate <= dateFilterMax;
+        } else {
+          return flight;
+        }
+      })
+      .filter((flight, index) => {
+        const fromIndex = (this.pagination - 1) * 20;
+        const toIndex = this.pagination * 20;
+        return index >= fromIndex && index < toIndex;
+      });
+  }
+
+  paginationChange(direction: string) {
+    if (direction === 'next') {
+      this.pagination += 1;
+    } else {
+      this.pagination -= 1;
+    }
+    this.showFlights();
+    window.scroll(0, 0);
+  }
+
+  nextPageBtnControl() {
+    let tempLength = 0;
+    if (this.spaceXList) {
+      tempLength = this.spaceXList.length;
+    }
+    return Math.ceil(tempLength / 20) !== this.pagination;
+  }
+
+  backPageBtnControl() {
+    return this.pagination !== 1;
+  }
+
+  onSubmit() {
+    this.showFlights();
+  }
+
+  private getSpaceXList() {
+    this.spaceXListService.getSpaceXList().subscribe({
+      next: response => {
+        this.spaceXList = response;
+        console.log(response);
+        const tempDate = new Date();
+        this.showFlights();
+      },
+    });
   }
 }
